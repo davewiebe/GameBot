8c02aa2e2af3615c92da77b19c95d3b3fddc1d6e
Dave Wiebe <dawiebe@gmail.com>
2020-06-17 14:34:06 -0700

    Added basic bot functionality

---
+++ GameBot/Modules/Commands.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+    }
+}
