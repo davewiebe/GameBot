f3efefe4d5749175e1123076e498d1a4c1fd9690
Jon Vandermey <jonvandermey@gmail.com>
2020-12-02 21:02:02 -0800

    Changed parsing logic to default to include wilds unless specified

--- PerudoBotTests/Claims/ParseClaimShould.cs
+++ PerudoBotTests/Claims/ParseClaimShould.cs
@@ -11,10 +11,12 @@ namespace PerudoBotTests
     [TestFixture]
     public class ParseClaimShould
     {
-        [TestCase("1 3", Operator.Exactly, 1, 3, false)]
-        [TestCase("1 34", Operator.Exactly, 1, 34, false)]
+        [TestCase("1 3!", Operator.Exactly, 1, 3, false)]
+        [TestCase("1 3!", Operator.Exactly, 1, 3, false)]
+        [TestCase("2 3s!", Operator.Exactly, 2, 3, false)]
+        [TestCase("2 3nowilds", Operator.Exactly, 2, 3, false)]
         [Test]
-        public void HandleClaimsWithOperatorsAndNoWilds(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
+        public void HandleClaimsWithNoOperatorsAndNoWilds(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
         {
             var parsedClaim = ClaimParser.Parse(claimText);
 
@@ -24,9 +26,9 @@ namespace PerudoBotTests
             parsedClaim.IncludeWilds.ShouldBe(expectedIncludeWilds);
         }
 
-        [TestCase("<1 3", Operator.LessThan, 1, 3, false)]
-        [TestCase("<2 4", Operator.LessThan, 2, 4, false)]
-        [TestCase("<4 6", Operator.LessThan, 4, 6, false)]
+        [TestCase("<1 3!", Operator.LessThan, 1, 3, false)]
+        [TestCase("<2 4!", Operator.LessThan, 2, 4, false)]
+        [TestCase("<4 6nowilds", Operator.LessThan, 4, 6, false)]
         [Test]
         public void HandleClaimsWithLessThanOperatorAndNoWilds(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
         {
@@ -38,8 +40,8 @@ namespace PerudoBotTests
             parsedClaim.IncludeWilds.ShouldBe(expectedIncludeWilds);
         }
 
-        [TestCase(">1 3", Operator.GreaterThan, 1, 3, false)]
-        [TestCase(">3 4", Operator.GreaterThan, 3, 4, false)]
+        [TestCase(">1 3!", Operator.GreaterThan, 1, 3, false)]
+        [TestCase(">3 4!", Operator.GreaterThan, 3, 4, false)]
         [Test]
         public void HandleClaimsWithGreaterThanOperatorAndNoWilds(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
         {
@@ -51,8 +53,8 @@ namespace PerudoBotTests
             parsedClaim.IncludeWilds.ShouldBe(expectedIncludeWilds);
         }
 
-        [TestCase("~1 1*", Operator.Approximately, 1, 1, true)]
-        [TestCase("~4 3", Operator.Approximately, 11, 3, false)]
+        [TestCase("~1 1", Operator.Approximately, 1, 1, true)]
+        [TestCase("~4 3!", Operator.Approximately, 4, 3, false)]
         [Test]
         public void HandleClaimsWithTildeOperator(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
         {
@@ -64,8 +66,8 @@ namespace PerudoBotTests
             parsedClaim.IncludeWilds.ShouldBe(expectedIncludeWilds);
         }
 
-        [TestCase("1 1*", Operator.Exactly, 1, 1, true)]
-        [TestCase("11 3*", Operator.Exactly, 11, 3, true)]
+        [TestCase("1 1", Operator.Exactly, 1, 1, true)]
+        [TestCase("11 3", Operator.Exactly, 11, 3, true)]
         [Test]
         public void HandleClaimsThatIncludeWilds(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
         {
@@ -77,11 +79,11 @@ namespace PerudoBotTests
             parsedClaim.IncludeWilds.ShouldBe(expectedIncludeWilds);
         }
 
-        [TestCase("1 1*", Operator.Exactly, 1, 1, true)]
-        [TestCase("11 3", Operator.Exactly, 11, 3, false)]
-        [TestCase(">3 4*", Operator.GreaterThan, 3, 4, true)]
-        [TestCase("<1 1", Operator.LessThan, 1, 1, false)]
-        [TestCase("~4 2", Operator.Approximately, 4, 2, false)]
+        [TestCase("1 1", Operator.Exactly, 1, 1, true)]
+        [TestCase("11 3!", Operator.Exactly, 11, 3, false)]
+        [TestCase(">3 4", Operator.GreaterThan, 3, 4, true)]
+        [TestCase("<1 1!", Operator.LessThan, 1, 1, false)]
+        [TestCase("~4 2!", Operator.Approximately, 4, 2, false)]
         [Test]
         public void HandleClaimsWithAnyOperatorAndVariableIncludeWilds(string claimText, Operator expectedOperator, int expectedQuantity, int expectedPips, bool expectedIncludeWilds)
         {
