3102c4ccbac1090cc40642e59b4391510d5f4966
Jon Vandermey <jonvandermey@gmail.com>
2021-01-25 14:52:43 -0800

    Re-added leaderboards after mistakenly overwriting them with Elo leaderboards

--- PerudoBot/Modules/ScoreboardCommands.cs
+++ PerudoBot/Modules/ScoreboardCommands.cs
@@ -136,6 +136,91 @@ namespace PerudoBot.Modules
             _ = await Context.Channel.SendMessageAsync(embed: embed).ConfigureAwait(false);
         }
 
+        [Command("leaderboard")]
+        [Alias("lb")]
+        public async Task Leaderboard(params string[] parameters)
+        {
+            var options = parameters.Select(o => o.ToLower());
+            var gameMode = "All Ranked Games";
+
+            var baseQuery = _db.GamePlayers.AsQueryable().AsNoTracking();
+
+            if (options.Any(o => o == "suddendeath"))
+            {
+                gameMode = "Sudden Death";
+                baseQuery = baseQuery.Where(p => p.Game.Penalty == 100);
+            }
+
+            if (options.Any(o => o == "variable"))
+            {
+                gameMode = "Variable Penalty";
+                baseQuery = baseQuery.Where(p => p.Game.Penalty == 0);
+            }
+
+            if (options.Any(o => o == "standard"))
+            {
+                gameMode = "Standard Penalty";
+                baseQuery = baseQuery.Where(p => p.Game.Penalty == 1);
+            }
+
+            var result = baseQuery.Where(p => p.Game.IsRanked)
+                .Where(p => p.Game.State == 3)
+                .Where(p => p.Game.GuildId == Context.Guild.Id)
+                .Where(p => !p.Player.IsBot)
+                .Select(p => new
+                {
+                    GameId = p.Game.Id,
+                    p.Game.IsRanked,
+                    p.Game.State,
+                    p.Game.DateFinished,
+                    p.Player.Username,
+                    p.Player.Nickname,
+                    // TODO: User GameUserId instead, when winner points to GamePlayerId
+                    IsWinner = (p.Player.Username == p.Game.Winner
+                        && p.Player.GuildId == p.Game.GuildId) ? 1 : 0,
+                    PlayerCount = p.Game.GamePlayers.Count()
+                })
+                .Where(p => p.PlayerCount >= 3 && p.PlayerCount <= 100)
+                .OrderByDescending(p => p.DateFinished)
+                .GroupBy(g => new { g.Username, g.Nickname })
+                .Select(g => new
+                {
+                    g.Key.Username,
+                    g.Key.Nickname,
+                    GamesPlayed = g.Count(),
+                    Wins = g.Sum(x => x.IsWinner),
+                    WinPercentage = ((double)g.Sum(x => x.IsWinner) / (double)g.Count()) * 100
+                })
+                .OrderByDescending(f => f.WinPercentage)
+                .ThenByDescending(f => f.GamesPlayed);
+
+            var usernamePadding = 13;
+            var gamesPlayedPadding = 5;
+            var winsPadding = 7;
+            var winPercentagePadding = 8;
+
+            var embedString = "Username".PadLeft(usernamePadding) + "GP".PadLeft(gamesPlayedPadding)
+                + "Wins".PadLeft(winsPadding) + "Win %".PadLeft(winPercentagePadding) + "\n";
+
+            foreach (var item in result)
+            {
+                var username = item.Nickname.PadLeft(usernamePadding);
+                var gamesPlayed = item.GamesPlayed.ToString().PadLeft(gamesPlayedPadding);
+                var wins = item.Wins.ToString().PadLeft(winsPadding);
+                var winPercentage = item.WinPercentage.ToString("0.0").PadLeft(winPercentagePadding);
+
+                embedString += $"{username}{gamesPlayed}{wins}{winPercentage}\n";
+            }
+
+            var builder = new EmbedBuilder()
+                                .WithTitle($"Leaderboard")
+                                .AddField(gameMode, $"```{embedString}```", inline: false)
+                                .AddField("Player count", "4+");
+            var embed = builder.Build();
+
+            _ = await Context.Channel.SendMessageAsync(embed: embed).ConfigureAwait(false);
+        }
+
         [Command("awards")]
         [Alias("records, walloffame")]
         public async Task AwardsAsync(params string[] parameters)
